f75bde362939e0cefc71e69e4a373cd0cc70dff5
0skarmp <omoralespj@gmail.com>
2023-10-22 20:38:12 +0000

    fix bugs

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-/* Script that prints the title */
 
 const request = require('request');
 
-const getURL = process.argv[2];
+const path = process.argv[2];
 
-request(getURL, (error, response) => { // configure the request
-  if (error) {
-    console.error('Error');
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+request(path, (error, res, body) => {
+  if (error) console.error(error);
+  else console.log(`code: ${res.statusCode}`);
 });
