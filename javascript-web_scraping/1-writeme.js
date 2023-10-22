d0e76d3ff91767b28b20208c6d8807e46a833053
0skarmp <omoralespj@gmail.com>
2023-10-22 20:04:37 +0000

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+/* Script that writes a string to a file */
+
 const fs = require('fs');
+const firstArg = process.argv[2];
+const content = process.argv[3];
 
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-try {
-  fs.writeFileSync(filePath, stringToWrite, 'utf-8');
-} catch (error) {
-  console.error(error);
-}
+fs.writeFile(firstArg, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
