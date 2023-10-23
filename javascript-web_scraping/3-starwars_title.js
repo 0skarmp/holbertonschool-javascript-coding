0a82ca90febf62d887018827e2d43515b6f19de8
0skarmp <omoralespj@gmail.com>
2023-10-23 03:34:48 +0000

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,35 @@
 #!/usr/bin/node
 
+// Import the 'request' module to make HTTP requests
 const request = require('request');
 
-const title = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${title}`;
+// Check if the correct number of arguments (3) is provided in the command line
+if (process.argv.length !== 3) {
+  console.error('Usage: node 3-starwars_title.js <movie_id>');
+  process.exit(1);
+}
 
-request(url, (error, res, body) => {
-  if (error) console.error(error);
-  else {
-    const data = JSON.parse(body);
-    console.log(data.title);
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+// Create the URL for the Star Wars API request
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  // Check if an error occurred during the request
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Check if the response status code is 200 (OK)
+  if (response.statusCode === 200) {
+    // Parse the JSON response
+    const movieData = JSON.parse(body);
+    // Print the title of the movie
+    console.log(movieData.title);
+  } else {
+    // Print an error message for unsuccessful requests.
+    console.error(`Error: Failed to retrieve movie data. Status code: ${response.statusCode}`);
+    process.exit(1);
   }
 });
