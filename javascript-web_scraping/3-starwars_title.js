a1339d22ab60e88c2aab0cf35852db71b346eaea
0skarmp <omoralespj@gmail.com>
2023-10-23 03:43:35 +0000

    task3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-// Get the movie ID from the command line argument
 const movieId = process.argv[2];
 
-// Define the Star Wars API endpoint
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Make an HTTP request to the API
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode === 200) {
+    console.error(error);
+  } else {
     const movie = JSON.parse(body);
     console.log(movie.title);
-  } else {
-    console.error('Request failed with status code:', response.statusCode);
   }
 });
