2000294062f8c4764380891608182d171a0bd96c
0skarmp <omoralespj@gmail.com>
2023-10-23 03:58:32 +0000

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
+/* script that print the number of movies */
 
 const request = require('request');
 
-const url = process.argv[2];
+const apiUrl = process.argv[2];
+const characterId = 18;
 
-request(url, (error, res, body) => {
-  if (error) console.error(error);
-  else {
-    const data = JSON.parse(body);
-    let i = 0;
-    let count = 0;
-    while (data.results[i]) {
-      data.results[i].characters.forEach((pj) => {
-        if (pj.endsWith('/18/')) count++;
-      });
-      i++;
-    }
-    console.log(count);
+request(apiUrl, (error, response, body) => { // configure the request
+  if (!error && response.statusCode === 200) { // check status code
+    const data = JSON.parse(body); // parse the response body as JSON
+    const movieName = data.results.filter((film) => // filter the movie
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
+    );
+
+    console.log(movieName.length);
+  } else {
+    console.error('Error:', error);
   }
 });
