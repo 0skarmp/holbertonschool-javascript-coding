463831c058e8c1c136721c4d3b245f94aaade398
0skarmp <omoralespj@gmail.com>
2023-10-23 03:52:41 +0000

    countj

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,20 @@
 
 const request = require('request');
 
-// Get the API URL from the command line argument
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Make an HTTP request to the API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode === 200) {
+request(url, (error, res, body) => {
+  if (error) console.error(error);
+  else {
     const data = JSON.parse(body);
-    const moviesWithWedgeAntilles = data.results.filter((movie) => {
-      return movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`);
-    });
-    console.log(moviesWithWedgeAntilles.length);
-  } else {
-    console.error('Request failed with status code:', response.statusCode);
+    let i = 0;
+    let count = 0;
+    while (data.results[i]) {
+      data.results[i].characters.forEach((pj) => {
+        if (pj.endsWith('/18/')) count++;
+      });
+      i++;
+    }
+    console.log(count);
   }
 });
