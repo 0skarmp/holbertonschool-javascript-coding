ff8edabac7dea8352c65e9306f0db084cc3b422f
0skarmp <omoralespj@gmail.com>
2023-10-23 04:11:14 +0000

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (!error) {
-    let length = 0;
-    for (const movie of JSON.parse(body).results) {
-      for (const char of movie.characters) {
-        if (char.includes(18)) {
-          length++;
-        }
-      }
+if (!url) {
+  console.error('You must provide a URL.');
+  process.exit(1);
+}
+
+request(url, (error, res, body) => {
+  if (error) console.error(error);
+  else {
+    const data = JSON.parse(body);
+    let i = 0;
+    let count = 0;
+    while (data.results[i]) {
+      data.results[i].characters.forEach((pj) => {
+        if (pj.endsWith('/18/')) count++;
+      });
+      i++;
     }
-    console.log(length);
+    console.log(count);
   }
 });
