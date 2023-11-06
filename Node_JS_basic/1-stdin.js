70e99c9df51d09040f115f241bccc830991168cb
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-11-05 22:12:31 -0500

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,8 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log("This important software is now closing");
-  rl.close();
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
