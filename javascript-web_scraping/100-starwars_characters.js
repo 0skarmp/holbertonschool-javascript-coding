726e5dec81a718135223190c74476ef0fcad9b26
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-11-29 20:53:41 -0500

    Create 100-starwars_characters.js

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Debes proporcionar el ID de la película como argumento.');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error('Error getting character data:', charError);
+        }
+      });
+    });
+  } else {
+    console.error('Error getting movie data:', error);
+  }
+});
